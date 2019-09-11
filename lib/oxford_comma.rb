1bfe3defa8607ded09c7086c688d804dc0e34955
Cody Dupuis <ctdupuis7@gmail.com>
2019-09-11 21:35:55 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
-def oxford_comma(array)
+require 'pry'
 
+def oxford_comma(array)
+  length = array.length.to_i
+  
+  
+    if length == 1 
+      return array.join(",")
+    elsif length == 2 
+      return array.join(" and ")
+    elsif length >= 3
+      array[-1].prepend("and ")
+      return array.join(", ")
+        binding.pry
+    end
 end
